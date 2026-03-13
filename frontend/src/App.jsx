d04975bd5472156c8b90814726c986249d16216f
jamesyuvaraj27 <jamesyuvaraj27@gmail.com>
2026-03-13 22:14:34 +0530

    Refactor App component to render LandingPageFlow instead of an empty div

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,8 @@
-import React from 'react'
+import React from 'react';
+import LandingPageFlow from './LandingPageFlow';
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <LandingPageFlow />;
+};
 
-export default App
+export default App;
